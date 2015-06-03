42ac22da3c130cfb6180da5868fbe2ccf9b31898
Alexander Norström <alexander@norstrom.org>
2015-06-03 14:52:44 +0200

    Prepared execute statement

--- src/main/scala/com/twitter/finagle/postgres/Client.scala
+++ src/main/scala/com/twitter/finagle/postgres/Client.scala
@@ -71,6 +71,21 @@ class Client(factory: ServiceFactory[PgRequest, PgResponse], id:String) {
     }
   }
 
+  def prepareAndExecute(sql: String, params: Any*): Future[OK] = {
+    val preparedStatement = factory.apply().flatMap {
+      service =>
+        parse(sql, Some(service)).map { name =>
+          new PreparedStatementImpl(name, service)
+        }
+    }
+
+    preparedStatement.flatMap {
+      statement =>
+        statement.exec(params: _*).ensure {
+          statement.closeService
+        }
+    }
+  }
   private[this] def sendQuery[T](sql: String)(handler: PartialFunction[PgResponse, Future[T]]) = {
     send(PgRequest(new Query(sql)))(handler)
   }
